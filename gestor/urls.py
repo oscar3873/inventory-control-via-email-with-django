75ad951ec4e5285407b52c316392a5fbd42fb188
oscar3873 <47361577+oscar3873@users.noreply.github.com>
2022-11-26 06:57:40 -0300

    algunos templates y staticos

--- gestor/urls.py
+++ gestor/urls.py
@@ -1,12 +1,16 @@
+from gestor.views import configurar, index
 from django.urls import path
+from gestor import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('productos/', views.ProductosListView.productos_list, name='productos'),
-    path('productos/proximos_a_vencer_', views.ProductosListView.productos_list, name='proximos_a_vencer'),
-    path('producto/<pk>', views.ProductoDetailView.as_view(), name='producto'),
-    path('producto/new/', views.ProductosListView.producto_new, name='producto_new'),
-    path('producto/delete/', views.ProductosListView.producto_delete, name='producto_delete'),
-    path('producto/editar/', views.ProductoDetailView.producto_editar, name='producto_editar'),
-    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('home',index, name='home'),
+    path('',index, name='home'),
+    path('productos/',views.ProductoListView.as_view(), name='productos'),
+    path('configurar/',configurar, name='configurar'),
+    # path('productos/proximos_a_vencer_', views.ProductosListView.productos_list, name='proximos_a_vencer'),
+    # path('producto/<pk>', views.ProductoDetailView.as_view(), name='producto'),
+    # path('producto/new/', views.ProductosListView.producto_new, name='producto_new'),
+    # path('producto/delete/', views.ProductosListView.producto_delete, name='producto_delete'),
+    # path('producto/editar/', views.ProductoDetailView.producto_editar, name='producto_editar'),
+    # path('settings/', views.SettingsView.as_view(), name='settings'),
 ]
--- gestor/views.py
+++ gestor/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from gestor.models import Producto
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+class ProductoListView(ListView):
+    model = Producto
+    paginate_by = 2
+    context_object_name = 'libros'
+    queryset = Producto.objects.all()
+    template_name='productos.html'
+
+def configurar(request):
+    return render(request, 'configurar.html')
