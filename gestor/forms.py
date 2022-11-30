376f2d37aae04c6d8612d33c5fc34dde368f77cb
CoFranso <cofranso.25@gmail.com>
2022-11-30 14:09:14 -0300

    ultimo commit dia 30\11\22

--- gestor/forms.py
+++ gestor/forms.py
@@ -6,7 +6,62 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
 
 class ProductoForm(forms.ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['stockDisp'].disabled = True
+
     class Meta:
         model = Producto
-        fields = ('marca', 'producto', 'tipo','fechaIngreso', 'fechaVnto','stockIng', 'stockDisp')
+        fields = ('marca', 'producto', 'tipo','fechaIngreso', 'fechaVnto','stockIng','codStock')
         widgets = {'fechaIngreso': NumberInput(attrs={'type': 'date'}),'fechaVnto': NumberInput(attrs={'type': 'date'}),}
+
+    def clean(self):
+        """ super(ProductoForm, self).clean() """
+        marca = self.cleaned_data.get('marca')
+        producto = self.cleaned_data.get('producto')
+        tipo = self.cleaned_data.get('tipo')
+        fechaIngreso = self.cleaned_data.get('fechaIngreso')
+        fechaVnto = self.cleaned_data.get('fechaVnto')
+        stockIng = self.cleaned_data.get('stockIng')
+        # stockDisp = self.cleaned_data.get('stockIng')
+        codigoBulto = self.cleaned_data.get('codStock')
+
+        if type(marca)==type(None):
+            self._errors['marca'] = self.error_class(['Campo obligatorio.'])
+
+        if type(producto) == type(None):
+            self._errors['producto'] = self.error_class(['Campo obligatorio.'])
+
+        if type(tipo) == type(None):
+            self._errors['tipo'] = self.error_class(['Campo obligatorio.'])
+
+        if fechaIngreso==None:
+            self._errors['fechaIngreso'] = self.error_class(['Campo obligatorio.'])
+
+        if fechaVnto==None:
+            self._errors['fechaVnto'] = self.error_class(['Campo obligatorio.'])
+
+        if fechaIngreso!=None and fechaVnto!=None:
+            if fechaVnto<fechaIngreso:
+                self._errors['fechaVnto'] = self.error_class(['La fecha de vencimiento debe ser mayor a la del ingreso.'])
+
+        if type(stockIng) == type(None):
+            self._errors['stockIng'] = self.error_class(['Campo obligatorio.'])
+        elif stockIng <= 0:
+            self._errors['stockIng'] = self.error_class(['El numero ingresado debe ser mayor a cero.'])
+
+        # if type(stockDisp) == type(None):
+        #     self._errors['stockDisp'] = self.error_class(['Campo obligatorio.'])
+        # elif stockDisp <= 0:
+        #     self._errors['stockDisp'] = self.error_class(['El numero ingresado debe ser mayor a cero.'])
+
+        # if type(stockIng) != type(None) and type(stockDisp) != type(None):
+        #     if stockIng < stockDisp:
+        #         self._errors['stockDisp'] = self.error_class(['El stock disponible debe ser menor o igual al stock de ingreso.'])
+
+        if type(codigoBulto) == type(None):
+            self._errors['codigoBulto'] = self.error_class(['Campo obligatorio.'])
+        elif codigoBulto == '':
+            self._errors['codigoBulto'] = self.error_class(['El numero ingresado debe ser mayor a cero.'])
+        
+        return self.cleaned_data
--- gestor/views.py
+++ gestor/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.list import ListView
 from gestor.forms import ProductoForm
+from django.views import generic
+from django.db.models import Q
 
 from gestor.models import Producto
 
@@ -14,11 +16,29 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-class ProductoListView(ListView):
-    model = Producto
-    context_object_name = 'productos'
-    queryset = Producto.objects.all()
-    template_name='productos.html'
+class ProductoListView(generic.ListView):
+    # model = Producto
+    # context_object_name = 'productos'
+    # queryset = Producto.objects.all()
+    # template_name='productos.html'
+
+    def producto_delete(request, pk):
+        prod = Producto.objects.get(pk=pk)
+        prod.delete()
+        return redirect('home')
+
+    def Buscar(request):
+
+        productos = Producto.objects.all()
+        busqueda= request.GET.get("myInput")
+        
+        if busqueda :
+            productos = Producto.objects.filter(
+                Q(marca__icontains = busqueda) |
+                Q(producto__icontains = busqueda) |
+                Q(codStock__icontains = busqueda)
+            )
+        return render(request, 'productos.html', {'productos' : productos} )
 
 def configurar(request):
     return render(request, 'configurar.html')
@@ -34,9 +54,13 @@ def producto_new(request):
             producto.fechaIngreso = formulario.cleaned_data['fechaIngreso']
             producto.fechaVnto = formulario.cleaned_data['fechaVnto']
             producto.stockIng = formulario.cleaned_data['stockIng']
-            producto.stockDisp = formulario.cleaned_data['stockDisp']
+            producto.stockDisp = formulario.cleaned_data['stockIng']
+            producto.codStock = formulario.cleaned_data['codStock']
             producto.save()
-            return redirect('productos')
+            
+            return redirect('home')
+        else:
+            return render(request, 'producto_new.html',{'formulario':formulario})
     else:
         formulario = ProductoForm()
     return render(request, 'producto_new.html', {'formulario': formulario})
@@ -47,9 +71,16 @@ def producto_update(request, pk):
         formulario = ProductoForm(request.POST, instance=producto)
         if formulario.is_valid():
             producto = formulario.save(commit=False)
-            producto.nombre = formulario.cleaned_data['nombre']
+            producto.marca = formulario.cleaned_data['marca']
+            producto.producto = formulario.cleaned_data['producto']
+            producto.tipo = formulario.cleaned_data['tipo']
+            producto.fechaIngreso = formulario.cleaned_data['fechaIngreso']
+            producto.fechaVnto = formulario.cleaned_data['fechaVnto']
+            producto.stockIng = formulario.cleaned_data['stockIng']
+            producto.stockDisp = formulario.cleaned_data['stockIng']
+            producto.codStock = formulario.cleaned_data['codStock']
             producto.save()
-            return redirect('generos')
+            return redirect('home')
     else:
         formulario = ProductoForm(instance=producto)
     
