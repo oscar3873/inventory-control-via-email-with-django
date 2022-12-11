5255eb0632fd10252087135754f566370e893241
oscar3873 <47361577+oscar3873@users.noreply.github.com>
2022-12-11 04:18:33 -0300

    refactor code

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestor.urls') )
-] #+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
     
 if settings.DEBUG:
--- gestor/static/alerthide.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#myWish").click(function showAlert() {
-        $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
-            $("#success-alert").slideUp(500);
-        });
-    });
-});
